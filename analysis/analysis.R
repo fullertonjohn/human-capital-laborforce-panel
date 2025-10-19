7fbb0b718c4ffc4669c18bf2181eb79e3c7c79c4
John Fullerton <johnfull@umich.edu>
2025-10-19 13:50:50 -0400

    extended regression to 2019, updated outputs

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -6,7 +6,6 @@ library(janitor)
 #import, preview data
 data <- read_csv("data/merged_state_panel.csv")
 glimpse(data)
-rm(df)
 summary(data)
 #compute Pearson correlation coefficient
 cor(data$lfp_sa, data$bachelors_share, use = "complete.obs")
@@ -15,7 +14,7 @@ ggplot(data, aes(x = bachelors_share, y = lfp_sa)) +
   geom_point(alpha = 0.6) +
   geom_smooth(method = "lm", se = TRUE, color = "darkred") +
   labs(
-    title = "Share of Population with at least a Bachelor's Degree vs Labor Force Participation rate (2010-2018)",
+    title = "Share of Population with at least a Bachelor's Degree vs Labor Force Participation rate (2010-2019)",
     x = "% of population with a Bachelor's degree or Higher",
     y = "Labor Force Participation Rate (%)"
   ) + 
@@ -30,10 +29,10 @@ ggplot(data, aes(x = bachelors_share, y = lfp_sa)) +
   library(modelsummary)
 
   # Export both side-by-side as an HTML table
-  modelsummary(
-    list("OLS (Pooled)" = lm_model,
-         "Two-way Fixed Effects" = model_fe),
-    output = "outputs/regression_summary.html"
-  )
+   modelsummary(
+     list("OLS (Pooled)" = lm_model,
+          "Two-way Fixed Effects" = model_fe),
+     output = "outputs/regression_summary.html"
+   )
 
   
